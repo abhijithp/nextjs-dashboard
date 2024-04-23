41cc3659b92fb99fb1e8e9f5323863d28ce9374d
Purushan Abhijith (BRT-SG) <abhijith.purushan@brtchip.com>
2024-04-23 17:28:37 +0800

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.js
 
-module.exports = nextConfig;
+module.exports = {
+    async rewrites() {
+      return [
+        {
+          source: '/dashboard',
+          destination: '/app/dashboard/page', // Path to the actual file
+        },
+      ];
+    },
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/dashboard', // The path you want users to be redirected to
+          permanent: true, // Set to true if the redirect should be permanent
+        },
+      ];
+    },
+  };
+  
